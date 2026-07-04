982a93a07f3744a8f2edc990c7aebc7eef2faf2f
ert516106-code <ert516106@gmail.com>
2026-07-05 07:49:11 +0800

    Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 import { Button } from "../components/ui/button";
-import Input from "../components/ui/input";
+import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
 
 import {
