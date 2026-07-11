7155638f030fcd18abdd0532ecb57af6b1869bdf
ert516106-code <ert516106@gmail.com>
2026-07-11 08:18:08 +0800

    main.jsx
    Removed AuthProvider wrapper from App component.

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,12 @@
-```jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { AuthProvider } from "./auth-v2/AuthContext";
-
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 );
-```
