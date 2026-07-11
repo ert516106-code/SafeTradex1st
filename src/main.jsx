ce59efa215ad7469d4baf179c76f17a2754eaf89
ert516106-code <ert516106@gmail.com>
2026-07-11 08:06:15 +0800

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
+```jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import { AuthProvider } from "./auth-v2/AuthContext";
+
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
+```
