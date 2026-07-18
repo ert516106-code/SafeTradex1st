e18a3b6cbcc53df0b87080cfe1d16540ee48a0b5
ert516106-code <ert516106@gmail.com>
2026-07-18 14:08:39 +0800

    Add Profile route with ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Deposit from "./pages/Deposit";
 import Transfer from "./pages/Transfer";
 import Financial from "./pages/Financial";
 import Assets from "./pages/Assets";
+import Profile from "./pages/Profile";
 
 import ProtectedRoute from "./routes/ProtectedRoute";
 import { MarketProvider } from "./contexts/MarketContext";
@@ -91,6 +92,15 @@ export default function App() {
             element={
               <ProtectedRoute>
                 <Assets />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
               </ProtectedRoute>
             }
           />
