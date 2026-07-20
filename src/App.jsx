a9d4259c9b693b4c01d9d690482d7aa701654d4a
ert516106-code <ert516106@gmail.com>
2026-07-20 23:38:01 +0800

    Add new routes for various settings and pages

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,17 @@ import Financial from "./pages/Financial";
 import Assets from "./pages/Assets";
 import Notifications from "./pages/Notifications";
 
+import SecurityCenter from "./pages/SecurityCenter";
+import PersonalInformation from "./pages/PersonalInformation";
+import WalletAddresses from "./pages/WalletAddresses";
+import NotificationSettings from "./pages/NotificationSettings";
+import LanguageSettings from "./pages/LanguageSettings";
+import AppearanceSettings from "./pages/AppearanceSettings";
+import HelpCenter from "./pages/HelpCenter";
+import TermsPrivacy from "./pages/TermsPrivacy";
+import AboutSafeTrade from "./pages/AboutSafeTrade";
+import RewardsCenter from "./pages/RewardsCenter";
+
 import ProtectedRoute from "./routes/ProtectedRoute";
 import { MarketProvider } from "./contexts/MarketContext";
 
@@ -25,110 +36,33 @@ export default function App() {
       <MarketProvider>
         <Routes>
           <Route path="/" element={<Splash />} />
-
           <Route path="/login" element={<Login />} />
-
           <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
 
-          <Route
-            path="/forgot-password"
-            element={<ForgotPassword />}
-          />
+          <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/markets" element={<ProtectedRoute><Markets /></ProtectedRoute>} />
+          <Route path="/coin/:id" element={<ProtectedRoute><CoinDetails /></ProtectedRoute>} />
+          <Route path="/deposit/*" element={<ProtectedRoute><Deposit /></ProtectedRoute>} />
+          <Route path="/withdraw/*" element={<ProtectedRoute><Withdraw /></ProtectedRoute>} />
+          <Route path="/transfer/*" element={<ProtectedRoute><Transfer /></ProtectedRoute>} />
+          <Route path="/convert/*" element={<ProtectedRoute><Convert /></ProtectedRoute>} />
+          <Route path="/financial" element={<ProtectedRoute><Financial /></ProtectedRoute>} />
+          <Route path="/assets" element={<ProtectedRoute><Assets /></ProtectedRoute>} />
+          <Route path="/notifications" element={<ProtectedRoute><Notifications /></ProtectedRoute>} />
 
-          <Route
-            path="/home"
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
+          <Route path="/security-center" element={<ProtectedRoute><SecurityCenter /></ProtectedRoute>} />
+          <Route path="/personal-information" element={<ProtectedRoute><PersonalInformation /></ProtectedRoute>} />
+          <Route path="/wallet-addresses" element={<ProtectedRoute><WalletAddresses /></ProtectedRoute>} />
+          <Route path="/notification-settings" element={<ProtectedRoute><NotificationSettings /></ProtectedRoute>} />
+          <Route path="/language-settings" element={<ProtectedRoute><LanguageSettings /></ProtectedRoute>} />
+          <Route path="/appearance-settings" element={<ProtectedRoute><AppearanceSettings /></ProtectedRoute>} />
+          <Route path="/help-center" element={<ProtectedRoute><HelpCenter /></ProtectedRoute>} />
+          <Route path="/terms-privacy" element={<ProtectedRoute><TermsPrivacy /></ProtectedRoute>} />
+          <Route path="/about-safetrade" element={<ProtectedRoute><AboutSafeTrade /></ProtectedRoute>} />
+          <Route path="/rewards-center" element={<ProtectedRoute><RewardsCenter /></ProtectedRoute>} />
 
-          <Route
-            path="/markets"
-            element={
-              <ProtectedRoute>
-                <Markets />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/coin/:id"
-            element={
-              <ProtectedRoute>
-                <CoinDetails />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/deposit/*"
-            element={
-              <ProtectedRoute>
-                <Deposit />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/withdraw/*"
-            element={
-              <ProtectedRoute>
-                <Withdraw />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/transfer/*"
-            element={
-              <ProtectedRoute>
-                <Transfer />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/convert/*"
-            element={
-              <ProtectedRoute>
-                <Convert />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/financial"
-            element={
-              <ProtectedRoute>
-                <Financial />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/assets"
-            element={
-              <ProtectedRoute>
-                <Assets />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/notifications"
-            element={
-              <ProtectedRoute>
-                <Notifications />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="*"
-            element={<Navigate to="/" replace />}
-          />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </MarketProvider>
     </BrowserRouter>
