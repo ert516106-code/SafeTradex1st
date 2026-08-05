f629b17c39944160e087a6d94edd88c1e7f7d07a
ert516106-code <ert516106@gmail.com>
2026-08-05 11:28:29 +0800

    Wrap App component with LanguageProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import "./styles/global.css";
-
 import App from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
+import { LanguageProvider } from "./contexts/LanguageContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
     </AuthProvider>
   </React.StrictMode>
 );
